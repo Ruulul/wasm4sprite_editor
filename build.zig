a2bc12e0f00a5ae3463b32f8f8656cc8aa164a49
Ruulul <vamjunior01@gmail.com>
2023-11-19 18:48:49 -0300

    initial commit

---
+++ build.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const lib = b.addExecutable(.{
+        .name = "cart",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = .{ .cpu_arch = .wasm32, .os_tag = .freestanding },
+        .optimize = std.builtin.OptimizeMode.ReleaseSmall,
+    });
+
+    lib.import_memory = true;
+    lib.initial_memory = 65536;
+    lib.max_memory = 65536;
+    lib.stack_size = 14752;
+
+    // Export WASM-4 symbols
+    lib.export_symbol_names = &[_][]const u8{ "start", "update" };
+
+    b.installArtifact(lib);
+
+    const run_cmd = b.addSystemCommand(
+        &.{
+            "w4",
+            "run-native",
+            "zig-out/bin/cart.wasm",
+        },
+    );
+    run_cmd.step.dependOn(&lib.step);
+
+    const run_step = b.step("run", "run the cart");
+    run_step.dependOn(&run_cmd);
+}
