996d4bcbf8e3a466a20be8305c0c14b4f236b71d
Ruulul <vamjunior01@gmail.com>
2023-11-19 20:54:49 -0300

    mouse input

--- src/main.zig
+++ src/main.zig
@@ -54,7 +54,7 @@ const Selection = enum {
     }
 };
 var mode: Mode = .@"1BBP";
-var size: Size = .x16;
+var size: Size = .x8;
 var selection: Selection = .mode;
 var tile: usize = 0;
 var buffer = [_]u2{ 0 } ** (64 * 64);
@@ -72,42 +72,65 @@ export fn start() void {
 }
 
 var prev_input: u8 = 0;
-fn handleInput(input: u8) void {
+var prev_mouse: u8 = 0;
+fn handleInput() void {
+    const input = w4.GAMEPAD1.*;
     defer prev_input = input;
+    const mouse = w4.MOUSE_BUTTONS.*;
+    defer prev_mouse = mouse;
     const just_pressed = (prev_input ^ input) & input;
+    const just_pressed_mouse = (prev_mouse ^ mouse) & mouse;
     if (just_pressed & w4.BUTTON_DOWN != 0) selection = selection.next();
     if (just_pressed & w4.BUTTON_UP != 0) selection = selection.prev();
-    if (input & w4.BUTTON_RIGHT != 0) tile +%= 1;
-    if (input & w4.BUTTON_LEFT != 0) tile -%= 1;
+    if (just_pressed & w4.BUTTON_RIGHT != 0) tile +%= 1;
+    if (just_pressed & w4.BUTTON_LEFT != 0) tile -%= 1;
     
-    if (just_pressed & w4.BUTTON_1 != 0) {
+    if (just_pressed & w4.BUTTON_2 != 0) {
         switch (selection) {
             .mode => mode = mode.next(),
             .size => size = size.next(),
-            else => {}
+            else => buffer[tileIndex()] +%= 1,
         }
+    }
+
+    if (just_pressed_mouse & w4.MOUSE_LEFT != 0) {
+        const x = normalizeCoord(w4.MOUSE_X.*);
+        const y = normalizeCoord(w4.MOUSE_Y.*);
+        if (x < 16 or y < 24) return;
+        const x_with_offset: usize = @intCast(x - 16);
+        const y_with_offset: usize = @intCast(y - 24);
+        
+        const side = @intFromEnum(size);
+        const x_tile = @divTrunc(x_with_offset, @as(usize, @intCast(size.factor())));
+        const y_tile = @divTrunc(y_with_offset, @as(usize, @intCast(size.factor())));
+        buffer[x_tile + y_tile * side] +%= 1;
     }
 }
 
 export fn update() void {
     frame += 1;
-    handleInput(w4.GAMEPAD1.*);
+    handleInput();
     checkSelection(.mode);
     w4.textPrint(2, "Mode: {s}", 3, 3, .{@tagName(mode)}) catch {};
     checkSelection(.size);
     w4.textPrint(2, "Size: {s}", 3, 13, .{@tagName(size)}) catch {};
     checkSelection(.tile);
-    w4.rect(16, 24, 128, 128);
     const factor = size.factor();
     const side = @intFromEnum(size);
     for (0..side) |x| for (0..side) |y| {
         const x_offset: i32 = @intCast(x);
         const y_offset: i32 = @intCast(y);
         
-        w4.DRAW_COLORS.* = if (isTile(x, y)) 0x1 else 0x0;
+        w4.DRAW_COLORS.* = @as(u16, switch (mode) {
+            .@"1BBP" => buffer[x + y * side] % 2,
+            .@"2BBP" => buffer[x + y * side],
+        }) + 1;
 
         w4.rect(16 + x_offset * factor, 24 + y_offset * factor, @as(u32, @intCast(factor)), @as(u32, @intCast(factor)));
     };
+    w4.DRAW_COLORS.* = 0x40;
+    w4.rect(15, 23, 129, 129);
+    w4.DRAW_COLORS.* = 0x2;
 }
 
 fn checkSelection(to_check: Selection) void {
@@ -120,4 +143,12 @@ fn checkSelection(to_check: Selection) void {
 fn isTile(x: usize, y: usize) bool {
     const side = @intFromEnum(size);
     return (tile % (side * side)) == (x + y * side);
+}
+fn tileIndex() usize {
+    const side = @intFromEnum(size);
+    return tile % (side * side);
+}
+
+fn normalizeCoord(n: i16) i32 {
+    return @divTrunc(n * 2, 3);
 }
