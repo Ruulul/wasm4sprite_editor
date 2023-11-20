6b2350c59d0df022cdc58739a83b3a8f04a9dbb7
Ruulul <vamjunior01@gmail.com>
2023-11-20 11:26:56 -0300

    file save

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,11 @@
 const std = @import("std");
 const w4 = @import("wasm4.zig");
 
-const Mode = enum {
-    @"1BBP",
-    @"2BBP",
+const Mode = enum(u32) {
+    @"1BBP" = w4.BLIT_1BPP,
+    @"2BBP" = w4.BLIT_2BPP,
     fn next(self: Mode) Mode {
-        return switch(self) {
+        return switch (self) {
             .@"1BBP" => .@"2BBP",
             .@"2BBP" => .@"1BBP",
         };
@@ -17,7 +17,7 @@ const Size = enum(usize) {
     x32 = 32,
     x64 = 64,
     fn next(self: Size) Size {
-        return switch(self) {
+        return switch (self) {
             .x8 => .x16,
             .x16 => .x32,
             .x32 => .x64,
@@ -25,7 +25,7 @@ const Size = enum(usize) {
         };
     }
     fn factor(self: Size) i32 {
-        return switch(self) {
+        return switch (self) {
             .x64 => 2,
             .x32 => 4,
             .x16 => 8,
@@ -49,7 +49,6 @@ const Selection = enum {
             .mode => .tile,
             .tile => .size,
             .size => .mode,
-
         };
     }
 };
@@ -57,8 +56,9 @@ var mode: Mode = .@"1BBP";
 var size: Size = .x8;
 var selection: Selection = .mode;
 var tile: usize = 0;
-var buffer = [_]u2{ 0 } ** (64 * 64);
+var buffer = [_]u2{0} ** (64 * 64);
 var frame: u64 = 0;
+var last_sprite: [1024]u8 = undefined;
 
 const rusty_palette = .{
     0x000000,
@@ -68,7 +68,7 @@ const rusty_palette = .{
 }; // https://lospec.com/palette-list/rusty-metal-g
 
 export fn start() void {
-    w4.PALETTE.* = rusty_palette;
+    _ = w4.diskr(&last_sprite, 1024);
 }
 
 var prev_input: u8 = 0;
@@ -84,7 +84,28 @@ fn handleInput() void {
     if (just_pressed & w4.BUTTON_UP != 0) selection = selection.prev();
     if (just_pressed & w4.BUTTON_RIGHT != 0) tile +%= 1;
     if (just_pressed & w4.BUTTON_LEFT != 0) tile -%= 1;
-    
+
+    if (just_pressed & w4.BUTTON_1 != 0) {
+        const div_factor: usize = switch (mode) {
+            .@"1BBP" => 8,
+            .@"2BBP" => 4,
+        };
+        const mul_factor: usize = switch (mode) {
+            .@"1BBP" => 1,
+            .@"2BBP" => 2,
+        };
+        const add_factor: usize = mul_factor - 1;
+        const side = @intFromEnum(size);
+        @memset(&last_sprite, 0);
+        _ = w4.diskw(&last_sprite, 1024);
+        for (buffer[0..(side * side)], 0..) |pixel, i| {
+            const byte_index = i / div_factor;
+            var bit_index: u3 = @intCast((i % div_factor) * mul_factor + add_factor);
+            const bit_value = @as(u8, pixel) << (7 - bit_index);
+            last_sprite[byte_index] |= bit_value;
+            _ = w4.diskw(&last_sprite, side * side);
+        }
+    }
     if (just_pressed & w4.BUTTON_2 != 0) {
         switch (selection) {
             .mode => mode = mode.next(),
@@ -102,7 +123,7 @@ fn handleInput() void {
         if (x < 16 or y < 24) return;
         const x_with_offset: usize = @intCast(x - 16);
         const y_with_offset: usize = @intCast(y - 24);
-        
+
         const side = @intFromEnum(size);
         const x_tile = @divTrunc(x_with_offset, @as(usize, @intCast(size.factor())));
         const y_tile = @divTrunc(y_with_offset, @as(usize, @intCast(size.factor())));
@@ -114,18 +135,27 @@ fn handleInput() void {
 
 export fn update() void {
     frame += 1;
+    const factor = size.factor();
+    const side = @intFromEnum(size);
+    w4.DRAW_COLORS.* = 0x4321;
+    w4.blit(
+        &last_sprite,
+        160 - @as(i32, @intCast(side)),
+        0,
+        @intCast(side),
+        @intCast(side),
+        @intFromEnum(mode),
+    );
     handleInput();
     checkSelection(.mode);
     w4.textPrint(2, "Mode: {s}", 3, 3, .{@tagName(mode)}) catch {};
     checkSelection(.size);
     w4.textPrint(2, "Size: {s}", 3, 13, .{@tagName(size)}) catch {};
     checkSelection(.tile);
-    const factor = size.factor();
-    const side = @intFromEnum(size);
     for (0..side) |x| for (0..side) |y| {
         const x_offset: i32 = @intCast(x);
         const y_offset: i32 = @intCast(y);
-        
+
         w4.DRAW_COLORS.* = @as(u16, switch (mode) {
             .@"1BBP" => buffer[x + y * side] % 2,
             .@"2BBP" => buffer[x + y * side],
